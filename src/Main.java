f2b097415f8c1d7d819f878e8c07cf706e4c7c86
leilei-li <lileilei258@qq.com>
2017-03-15 16:38:12 +0800

    增加hello world

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int a;
-        int b;
-        while (in.hasNext()) {
-            a = in.nextInt();
-            b = in.nextInt();
-            System.out.println(a + b);
-        }
+        System.out.println("hello world");
     }
 }
