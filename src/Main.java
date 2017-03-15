257d9ebebc6a609d05854d49b461a5f660598c2b
leilei-li <lileilei258@qq.com>
2017-03-15 16:28:31 +0800

    九度OJ 第一题 a+b 练手用的

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Myrunnable runnable = new Myrunnable();
-        Thread t1 = new Thread(runnable);
-        Thread t2 = new Thread(runnable);
-        t1.setName("t1");
-        t1.start();
-        t2.setName("t2");
-        t2.start();
-
+        Scanner in = new Scanner(System.in);
+        int a;
+        int b;
+        while (in.hasNext()) {
+            a = in.nextInt();
+            b = in.nextInt();
+            System.out.println(a + b);
+        }
     }
 }
--- src/Myrunnable.java
+++ src/Myrunnable.java
@@ -2,42 +2,19 @@
  * Created by lileilei on 2017/3/8.
  */
 public class Myrunnable implements Runnable {
-    private int a = 10;
+    private int a = 0;
 
     @Override
     public void run() {
         String tName = Thread.currentThread().getName();
-        if(tName.equals("t1")){
-            t1go();
-        }
-        if(tName.equals("t2")){
-            t2go();
-        }
-    }
-
-    private void t1go() {
-        String tName = Thread.currentThread().getName();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             System.out.println(tName + " " + a);
             a = a - 1;
-            try{
-                Thread.sleep(500);
-            }catch (Exception e){
+            try {
+                //Thread.sleep(500);
+            } catch (Exception e) {
 
             }
         }
-
-    }
-    private void t2go() {
-        String tName = Thread.currentThread().getName();
-        for (int i = 0; i < 100; i++) {
-            System.out.println("this is "+tName);
-            try{
-                Thread.sleep(500);
-            }catch (Exception e){
-
-            }
-        }
-
     }
 }
