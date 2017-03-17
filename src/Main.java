217aa48cab9db69754521f4db0f13bc7535e32e8
leilei-li <lileilei258@qq.com>
2017-03-17 15:36:31 +0800

    add QuickSort algorithm

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> s = new ArrayList<String>();
-        for (int i = 0; i < 10; i++) {
-            s.add("a");
-        }
-        for (int i = 0; i < s.size(); i++) {
-            System.out.println(s.get(i));
-        }
+        int[] num=new int[];
     }
 }
---
+++ src/Sort.java
@@ -0,0 +1,22 @@
+/**
+ * Created by lileilei on 2017/3/17.
+ */
+public class Sort {
+    public void QuickSort(int[] num, int low, int high) {
+        int mid = num[low];//将要拿来交换的值
+        int l = low;
+        int h = high;
+        while (l < h) {
+
+
+        }
+
+    }
+
+    public void Swap(int a, int b) {
+        int swap;
+        swap = a;
+        a = b;
+        b = swap;
+    }//swap a and b
+}
