81a510abbe99921b2b683e8fa6fa3a612607157d
leilei-li <lileilei258@qq.com>
2017-03-17 16:10:55 +0800

    add QiuckSort method

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] num=new int[];
+        int[] num = new int[]{1, 10, 2, 9, 3, 8, 4, 7, 5, 6};
+        Sort sort = new Sort();
+        sort.QuickSort(num, 0, 9);
+
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -3,20 +3,34 @@
  */
 public class Sort {
     public void QuickSort(int[] num, int low, int high) {
-        int mid = num[low];//将要拿来交换的值
         int l = low;
         int h = high;
+        int povit = num[low];
         while (l < h) {
+            while (l < h && num[h] >= povit)
+                h--;
+            if (l < h) {
+                int temp = num[h];
+                num[h] = num[l];
+                num[l] = temp;
+                l++;
+            }
+            while (l < h && num[l] <= povit)
+                l++;
 
-
+            if (l < h) {
+                int temp = num[h];
+                num[h] = num[l];
+                num[l] = temp;
+                h--;
+            }
         }
-
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
+        }
+        System.out.print("\n");
+        if (l > low) QuickSort(num, low, l - 1);
+        if (h < high) QuickSort(num, l + 1, high);
     }
 
-    public void Swap(int a, int b) {
-        int swap;
-        swap = a;
-        a = b;
-        b = swap;
-    }//swap a and b
 }
