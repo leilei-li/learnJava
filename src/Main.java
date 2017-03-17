0c5cc6979fcb6b5623f64379e08ea1c0c7e6cf58
leilei-li <lileilei258@qq.com>
2017-03-17 16:23:33 +0800

    add bubble sort

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         int[] num = new int[]{1, 10, 2, 9, 3, 8, 4, 7, 5, 6};
         Sort sort = new Sort();
-        sort.QuickSort(num, 0, 9);
-
+        //sort.QuickSort(num, 0, 9);
+        sort.BubbleSort(num);
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -31,6 +31,19 @@ public class Sort {
         System.out.print("\n");
         if (l > low) QuickSort(num, low, l - 1);
         if (h < high) QuickSort(num, l + 1, high);
-    }
+    }//quick sort
 
+    public void BubbleSort(int[] num) {
+        for (int i = 0; i < num.length; i++)
+            for (int j = 0; j < num.length; j++) {
+                if (num[i] < num[j]) {
+                    int temp = num[i];
+                    num[i] = num[j];
+                    num[j] = temp;
+                }
+            }
+        for (int i = 0; i < num.length; i++) {
+            System.out.print(num[i] + " ");
+        }
+    }
 }
