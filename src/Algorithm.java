bc711dd6d979ef063afba17877317806bcb2d936
leilei-li <lileilei258@qq.com>
2017-04-26 18:15:33 +0800

    新增背包问题

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -138,4 +138,69 @@ public class Algorithm {
         }
     }
 
+    public void fracKnapsack(int[] n, int[] w, int[] v, int W) {
+        int[] value = new int[n.length];//计算每件商品的单磅价值大小
+        int[] result = new int[n.length];//存储取用结果
+        value[0] = 0;
+        result[0] = 0;
+        for (int i = 1; i < n.length; i++) {
+            value[i] = v[i] / w[i];//计算每磅价值
+            n[i] = n[i] * w[i];//计算每件商品一共有多少磅
+            result[i] = 0;
+        }
+        while (W > 0) {
+            int pos = getMax(value);
+            if (n[pos] > 0) {
+                while (n[pos] > 0) {
+                    result[pos]++;
+                    n[pos]--;
+                    W--;
+                }
+            }
+            if (n[pos] == 0) {
+                value[pos] = 0;
+                continue;
+            }
+        }
+        int sum = 0;
+        System.out.println("拿走物品结果：");
+        for (int i = 1; i < result.length; i++) {
+            System.out.println("物品编号：" + i + " 数量：" + result[i] / w[i] + " 单价：" + v[i]);
+            sum = sum + (result[i] * v[i] / w[i]);
+        }
+        System.out.println("总价值为： " + sum);
+    }
+
+    private int getMax(int[] value) {//找出单磅价值最大的商品编号
+        int pos = 0;
+        int max = 0;
+        for (int i = 1; i < value.length; i++) {
+            if (value[i] != 0) {
+                if (max < value[i]) {
+                    max = value[i];
+                    pos = i;
+                }
+            }
+        }
+        return pos;
+    }
+
+    public void zeroOneKnapsack(int[] n, int[] w, int[] v, int W) {//带备忘的自顶向下法
+        int[][] result = new int[n.length][W + 1];//建立最优解矩阵，result[有前i种商品][背包容量W]
+        for (int i = 0; i <= W; i++) {
+            result[0][i] = 0;//初始化
+        }
+        for (int i = 1; i < n.length; i++) {//商品i种，从1开始
+            for (int j = 0; j <= W; j++) {//容量大小，从0到W
+                if (w[i] > j) {//无法放入背包，根据最优子结构可知
+                    result[i][j] = result[i - 1][j];
+                } else if (w[i] <= j) {//放入背包中
+                    int maxProfit = 0;
+                    maxProfit = Math.max(result[i - 1][j], result[i - 1][j - w[i]] + v[i]);//选择放还是不放
+                    result[i][j] = maxProfit;//存储最优解
+                }
+            }
+        }
+        System.out.println(result[n.length - 1][W]);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
 //        int[] r = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 //        String[] X = new String[]{"A", "B", "C", "B", "D", "A", "B"};
 //        String[] Y = new String[]{"B", "D", "C", "A", "B", "A"};
-        int[] s = new int[]{0, 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12};
-        int[] f = new int[]{0, 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16};//为了配合算法，下标都是从1开始到N+1
+//        int[] s = new int[]{0, 1, 3, 0, 5, 3, 5, 6, 8, 8, 2, 12};
+//        int[] f = new int[]{0, 4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16};//为了配合算法，下标都是从1开始到N+1
+        int[] n = new int[]{0, 2, 2, 2};//为了配合算法，下标都是从1开始,表示每件商品的数量
+        int[] w = new int[]{0, 10, 20, 30};//表示每件商品的重量
+        int[] v = new int[]{0, 60, 100, 120};//表示每件商品的价值
+        int W = 50;//背包的容积
 //        Sort sort = new Sort();
 //        sort.quickSort(num);
 //        sort.bubbleSort(num);
@@ -24,6 +28,8 @@ public class Main {
 //        search.binaryTreeSearch(num, 3);
 
         Algorithm algorithm = new Algorithm();
+//        algorithm.fracKnapsack(n, w, v, W);
+        algorithm.zeroOneKnapsack(n, w, v, W);
 //        int[] result = algorithm.getMaxSubArray(num, 0, 9);
 //        for (int i = 0; i < result.length; i++) {
 //            System.out.print(result[i] + " ");
@@ -32,7 +38,7 @@ public class Main {
 //            System.out.println(algorithm.memoizedCutRodAux(price, r, i));
 //            System.out.println(algorithm.bottomUpCutRod(price, r, i));
 //        }
-        algorithm.greedySelector(s, f);
+//        algorithm.greedySelector(s, f);
 
     }
 }
